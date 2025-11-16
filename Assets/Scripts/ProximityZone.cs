7b2c3f314b36fb74b1715d5c507ae2606c209b09
Alec Waxman <alecwaxman@gmail.com>
2025-11-16 11:29:08 +0200

    Clean up proximity zone

--- Assets/Scripts/ProximityZone.cs
+++ Assets/Scripts/ProximityZone.cs
@@ -9,8 +9,6 @@ public class ProximityZone : MonoBehaviour
     [Tooltip("The target object (sphere) to measure distance to")]
     public Transform targetObject;
 
-    // private Coroutine pulseCoroutine;
-
     public VideoPlayerWebGL videoPlayer;
 
     public GameObject videoQuad; // The animated version
@@ -50,8 +48,6 @@ public class ProximityZone : MonoBehaviour
         Debug.Log("Object has entered");
         if (other.CompareTag("MainCamera"))
         {
-            // Debug.Log("Player entered the proximity zone!");
-            // LogDistance(other.transform);
             currentState = ProximityState.EnteredZone;
         }
     }
@@ -80,27 +76,6 @@ public class ProximityZone : MonoBehaviour
         float distance = Vector3.Distance(playerTransform.position, targetObject.position);
         Debug.Log($"Distance to target: {distance:F2} meters");
     }
-
-    // private void StartPulsing()
-    // {
-    //     if (pulseCoroutine == null && targetObject != null)
-    //     {
-    //         pulseCoroutine = StartCoroutine(PulseScale(targetObject, 0.8f, 1.2f, 1f));
-    //     }
-    // }
-
-    // private void StopPulsing()
-    // {
-    //     if (pulseCoroutine != null)
-    //     {
-    //         StopCoroutine(pulseCoroutine);
-    //         pulseCoroutine = null;
-    //         if (targetObject != null)
-    //         {
-    //             targetObject.localScale = Vector3.one; // Reset scale
-    //         }
-    //     }
-    // }
 
     private void StartVideo()
     {
@@ -149,14 +124,10 @@ public class ProximityZone : MonoBehaviour
 
             case ProximityState.EnteredZone:
                 // Player has entered the zone
-                // Debug.Log("Player entered the proximity zone!");
                 currentState = ProximityState.MeasuringDistanceEnter;
                 break;
 
             case ProximityState.MeasuringDistanceEnter:
-                // LogDistance(gameObject.transform);
-                // Debug.Log((Camera.main.transform.position - gameObject.transform.position).magnitude.ToString());
-                // StopPulsing();
                 StopVideo();
                 // ^ move this to measuring distance exit
                 if (sqrDistance < sqrTrigger)
@@ -170,15 +141,11 @@ public class ProximityZone : MonoBehaviour
 
             case ProximityState.Triggered:
                 // Triggered state logic
-                // Debug.Log("Unity event triggered!");
-                // StartPulsing();
                 StartVideo();
                 currentState = ProximityState.MeasuringDistanceExit;
                 break;
 
             case ProximityState.MeasuringDistanceExit:
-                // LogDistance(gameObject.transform);
-                // Debug.Log((Camera.main.transform.position - gameObject.transform.position).magnitude.ToString());
                 if (sqrDistance > sqrExit)
                 {
                     // If the player is far enough from the target object, transition to ExitedZone
@@ -191,32 +158,8 @@ public class ProximityZone : MonoBehaviour
 
             case ProximityState.ExitedZone:
                 // Player has exited the zone
-                // Debug.Log("Player left the inner zone!");
                 currentState = ProximityState.Idle;
                 break;
         }
-        // This method can be used for additional logic if needed
     }
-
-    // private IEnumerator PulseScale(Transform obj, float minScale, float maxScale, float speed)
-    // {
-    //     float t = 0f;
-    //     bool growing = true;
-
-    //     while (true)
-    //     {
-    //         t += Time.deltaTime * speed;
-    //         float scale = growing ? Mathf.Lerp(minScale, maxScale, t) : Mathf.Lerp(maxScale, minScale, t);
-    //         obj.localScale = Vector3.one * scale;
-
-    //         if (t >= 1f)
-    //         {
-    //             t = 0f;
-    //             growing = !growing;
-    //         }
-
-    //         yield return null;
-    //     }
-    // }
-
 }
